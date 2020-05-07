ec4209e32cd8168107d72d58848f3e653b4bed94
huf0001 <samhuffer01@gmail.com>
2020-05-07 15:34:20 +1000

    Enemies went weird and weren't turning, so fixed that. Decided to be OCD about enemies spawning evenly across the map, rather than the four bands doubling up in the corners and I realise now I could have just done four bands that lock together in a circle -_- In any case the areas are now dynamically adjustable in-scene.

--- Assets/Scripts/Enemies/EnemyFactory.cs
+++ Assets/Scripts/Enemies/EnemyFactory.cs
@@ -11,12 +11,37 @@ public class EnemyFactory : MonoBehaviour
 
     //Serialized Fields----------------------------------------------------------------------------
 
+    [Header("Game Objects")]
     [SerializeField] private Enemy enemyPrefab;
     [SerializeField] private Transform enemyPoolParent;
+
+    [Header("Spawning Bounds")]
+    [SerializeField] private Transform outerTopLeft;
+    [SerializeField] private Transform outerBottomRight;
+    [SerializeField] private Transform innerTopLeft;
+    [SerializeField] private Transform innerBottomRight;
 
     //Non-Serialized Fields------------------------------------------------------------------------
 
     private List<Enemy> enemyPool;
+
+    float outerXMin;
+    float outerXMax;
+    float outerZMin;
+    float outerZMax;
+    float innerXMin;
+    float innerXMax;
+    float innerZMin;
+    float innerZMax;
+
+    float accumulativeAreaTopLeft;
+    float accumulativeAreaTopCentre;
+    float accumulativeAreaTopRight;
+    float accumulativeAreaCentreLeft;
+    float accumulativeAreaCentreRight;
+    float accumulativeAreaBottomLeft;
+    float accumulativeAreaBottomCentre;
+    float accumulativeAreaBottomRight;
 
     //PublicProperties-------------------------------------------------------------------------------------------------------------------------------
 
@@ -42,6 +67,35 @@ public class EnemyFactory : MonoBehaviour
 
         Instance = this;
         enemyPool = new List<Enemy>();
+
+        outerXMin = outerTopLeft.position.x;
+        outerXMax = outerBottomRight.position.x;
+        outerZMin = outerBottomRight.position.z;
+        outerZMax = outerTopLeft.position.z;
+        innerXMin = innerTopLeft.position.x;
+        innerXMax = innerBottomRight.position.x;
+        innerZMin = innerBottomRight.position.z;
+        innerZMax = innerTopLeft.position.z;
+        //Debug.Log($"outerXMin: {outerXMin}, outerXMax: {outerXMax}");
+        //Debug.Log($"outerZMin: {outerZMin}, outerZMax: {outerZMax}");
+        //Debug.Log($"innerXMin: {innerXMin}, innerXMax: {innerXMax}");
+        //Debug.Log($"innerZMin: {innerZMin}, innerZMax: {innerZMax}");
+
+        float leftXLength = innerXMin - outerXMin;
+        float centreXLength = innerXMax - innerXMin;
+        float rightXLength = outerXMax - innerXMax;
+        float bottomZLength = innerZMin - outerZMin;
+        float centreZLength = innerZMax - innerZMin;
+        float topZLength = outerZMax - innerZMax;
+
+        accumulativeAreaTopLeft = leftXLength * topZLength;
+        accumulativeAreaTopCentre = centreXLength * topZLength + accumulativeAreaTopLeft;
+        accumulativeAreaTopRight = rightXLength * topZLength + accumulativeAreaTopCentre;
+        accumulativeAreaCentreLeft = leftXLength * centreZLength + accumulativeAreaTopRight;
+        accumulativeAreaCentreRight = rightXLength * centreZLength + accumulativeAreaCentreLeft;
+        accumulativeAreaBottomLeft = leftXLength * bottomZLength + accumulativeAreaCentreRight;
+        accumulativeAreaBottomCentre = centreXLength * bottomZLength + accumulativeAreaBottomLeft;
+        accumulativeAreaBottomRight = rightXLength * bottomZLength + accumulativeAreaBottomCentre;
     }
 
     //Triggered Methods------------------------------------------------------------------------------------------------------------------------------
@@ -56,23 +110,39 @@ public class EnemyFactory : MonoBehaviour
         Enemy enemy;
         Vector3 spawnPoint;
 
-        switch (Random.Range(0, 4))
+        float selection = Random.Range(0, accumulativeAreaBottomRight);
+
+        if (selection <= accumulativeAreaTopLeft)
         {
-            case 0:
-                spawnPoint = new Vector3(Random.Range(-24, 24), 0.25f, Random.Range(-24, -18));
-                break;
-            case 1:
-                spawnPoint = new Vector3(Random.Range(-24, 24), 0.25f, Random.Range(18, 24));
-                break;
-            case 2:
-                spawnPoint = new Vector3(Random.Range(-24, -18), 0.25f, Random.Range(-24, 24));
-                break;
-            case 3:
-                spawnPoint = new Vector3(Random.Range(18, 24), 0.25f, Random.Range(-24, 24));
-                break;
-            default:
-                spawnPoint = Vector3.zero;
-                break;
+            spawnPoint = new Vector3(Random.Range(outerXMin, innerXMin), 0.5f, Random.Range(innerZMax, outerZMax));
+        }
+        else if (selection <= accumulativeAreaTopCentre)
+        {
+            spawnPoint = new Vector3(Random.Range(innerXMin, innerXMax), 0.5f, Random.Range(innerZMax, outerZMax));
+        }
+        else if (selection <= accumulativeAreaTopRight)
+        {
+            spawnPoint = new Vector3(Random.Range(innerXMax, outerXMax), 0.5f, Random.Range(innerZMax, outerZMax));
+        }
+        else if (selection <= accumulativeAreaCentreLeft)
+        {
+            spawnPoint = new Vector3(Random.Range(outerXMin, innerXMin), 0.5f, Random.Range(innerZMin, innerZMax));
+        }
+        else if (selection <= accumulativeAreaCentreRight)
+        {
+            spawnPoint = new Vector3(Random.Range(innerXMax, outerXMax), 0.5f, Random.Range(innerZMin, innerZMax));
+        }
+        else if (selection <= accumulativeAreaBottomLeft)
+        {
+            spawnPoint = new Vector3(Random.Range(outerXMin, innerXMin), 0.5f, Random.Range(outerZMin, innerZMin));
+        }
+        else if (selection <= accumulativeAreaBottomCentre)
+        {
+            spawnPoint = new Vector3(Random.Range(innerXMin, innerXMax), 0.5f, Random.Range(outerZMin, innerZMin));
+        }
+        else
+        {
+            spawnPoint = new Vector3(Random.Range(innerXMax, outerXMax), 0.5f, Random.Range(outerZMin, innerZMin));
         }
 
         if (enemyPool.Count > 0)
