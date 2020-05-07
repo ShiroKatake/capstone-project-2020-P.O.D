ba2bc8a1decb90f30900f24a14403f94c5a9f089
Thomas Middelmann <thomas@middelmanns.net>
2020-05-07 13:43:03 +1000

    Terrain generator fundementals created Editor variable fields for generator parameters

---
+++ Assets/Scripts/Terrain/Editor/TerrainGeneratorEditor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorEditor : Editor
+{
+    AnimationCurve blank = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    AnimationCurve curve = new AnimationCurve();
+
+    static int layers = 3;
+    static float smoothness = 0.01f;
+
+    static float noiseScale = 2f/100f;
+    static float centreFlatRadius = 50;
+
+    int values = 0;
+
+    public override void OnInspectorGUI() {
+
+        TerrainGenerator script = (TerrainGenerator)target;
+
+        var curveField = EditorGUI.CurveField(new Rect(10,0,EditorGUIUtility.currentViewWidth-35,100), curve);
+
+        EditorGUILayout.Space(100);
+
+        layers = EditorGUILayout.IntField("Layers", layers);
+        smoothness = EditorGUILayout.FloatField("Smoothness: ", smoothness);
+        noiseScale = EditorGUILayout.FloatField("Noise Scale: ", noiseScale);
+        centreFlatRadius = EditorGUILayout.FloatField("Centre Flat: ", centreFlatRadius);
+
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Generate Scripted Heightmap")) {
+
+            RedoCurve();
+            script.GenerateTerrain(curve, noiseScale, centreFlatRadius);
+
+        }
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Redo curve")) {
+            RedoCurve();
+        }
+        
+        
+    }
+
+    private void RedoCurve() {
+        curve = new AnimationCurve();
+
+        float tSeperation = 1 / (float)layers;
+        float oDelta = 1 / ((float)layers - 1);
+
+        float smoothVal = tSeperation * smoothness;
+
+        curve.AddKey(0, 0);
+
+        for (int i = 0; i < layers - 1; i++) {
+            Keyframe frame = new Keyframe((i + 1) * tSeperation - smoothVal, (i) * oDelta);
+            Keyframe frameUpper = new Keyframe((i + 1) * tSeperation + smoothVal, (i + 1) * oDelta);
+
+            curve.AddKey(frame);
+            curve.AddKey(frameUpper);
+        }
+
+        curve.AddKey(1, 1);
+
+        for (int i = 0; i < curve.keys.Length; i++) {
+            AnimationUtility.SetKeyLeftTangentMode(curve, i, AnimationUtility.TangentMode.Linear);
+            AnimationUtility.SetKeyRightTangentMode(curve, i, AnimationUtility.TangentMode.Linear);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Terrain GetTerrain() {
+        return GetComponent<Terrain>();
+    }
+
+    public void GenerateTerrain(AnimationCurve curve, float noiseScale, float centreFlatRadius) {
+        Terrain terrain = GetTerrain();
+        TerrainData data = terrain.terrainData;
+
+        int heightmapRes = data.heightmapResolution;
+
+        float[,] heights = new float[heightmapRes, heightmapRes];
+
+        Vector2 centre = new Vector2(heightmapRes / 2, heightmapRes / 2);
+
+        for (int xx = 0; xx < heightmapRes; xx++) {
+            for (int yy = 0; yy < heightmapRes; yy++) {
+                Vector2 pos = new Vector2(xx, yy);
+                float height;
+
+                if ((centre - pos).magnitude < centreFlatRadius) {
+                    height = curve.Evaluate(0.5f);
+                }
+                else {
+                    height = Mathf.PerlinNoise((float)xx * noiseScale + 0.1f, (float)yy * noiseScale + 0.1f);
+                    height = curve.Evaluate(height);
+                }
+                heights[xx, yy] = height;
+            }
+        }
+
+        data.SetHeights(0, 0, heights);
+
+    }
+
+}
