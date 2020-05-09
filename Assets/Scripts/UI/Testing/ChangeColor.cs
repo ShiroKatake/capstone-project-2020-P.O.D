b77ad4ce2fee41c5999ecd577dbac941463fd168
Cody <nilfalen1@gmail.com>
2020-05-09 15:56:26 +1000

    got a sprite to change color randomly via a fade method

---
+++ Assets/Scripts/UI/Testing/ChangeColor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private float fadeRate;
+    private float targetAlpha;
+    private Color curColor;
+    private Color newColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAlpha = image.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            curColor = image.color;
+            newColor = UnityEngine.Random.ColorHSV(0f,1f,1f,1f,0.5f,1f);
+        }
+        curColor = Color.Lerp(curColor, newColor, fadeRate*Time.deltaTime);
+        image.GetComponent<Image>().color = curColor;
+    }
+}
