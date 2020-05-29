d080eb9694d92494f3b20ed7ef044f9402e98194
Cody <nilfalen1@gmail.com>
2020-05-29 18:57:19 +1000

    semi-funcational Main Menu

---
+++ Assets/Scripts/UI/SceneSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField] private int MainGame;
+
+    public void OnTrigger(){
+        //print(MainGame.ToString());
+        SceneManager.LoadScene(MainGame);
+    }
+}
