baf214dd47a06b9d5c764d077483d2e0b99b3f3e
huf0001 <samhuffer01@gmail.com>
2020-07-03 18:52:44 +1000

    Got hovering according to terrain height working, made a couple of attempts with checking for cliffs, trying something new.

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Rewired;
+using System.Collections.Generic;
 using UnityEngine;
-using Rewired;
+using UnityEngine.AI;
 
 /// <summary>
 /// The player. Player controls the player's movement and shooting. For building spawning, see BuildingSpawningController.
@@ -12,11 +13,12 @@ public class PlayerMovementController : MonoBehaviour
     //Serialized Fields----------------------------------------------------------------------------
 
     [Header("Player Objects")]
-    [SerializeField] private Transform drone;
     [SerializeField] private Camera camera;
     [SerializeField] private Transform cameraTarget;
     [SerializeField] private Transform barrelTip;
     [SerializeField] private Transform barrelMagazine;
+    [SerializeField] private List<Transform> cliffDetectionMarkers;
+    [SerializeField] private List<Vector3> cliffTestOffsets;
 
     [Header("Player Stats")]
     [SerializeField] private float movementSpeed;
@@ -32,7 +34,8 @@ public class PlayerMovementController : MonoBehaviour
     //Variables for moving & determining if rotation is necessary
     private Vector3 movement;
     private Vector3 previousMovement = Vector3.zero;
-    private float hoverHeight;
+    private float defaultHoverHeight;
+    private LayerMask groundLayerMask;
 
     //Variables for rotating smoothly
     private Quaternion newRotation;
@@ -85,8 +88,9 @@ public class PlayerMovementController : MonoBehaviour
         health = GetComponent<Health>();
         rigidbody = GetComponent<Rigidbody>();
         timeOfLastShot = shootCooldown * -1;
-        hoverHeight = drone.position.y;
+        defaultHoverHeight = transform.position.y;
         gameOver = false;
+        groundLayerMask = LayerMask.GetMask("Ground");
     }
 
     /// <summary>
@@ -167,10 +171,7 @@ public class PlayerMovementController : MonoBehaviour
     /// </summary>
     private void Look()
     {
-        //Vector3 mousePos = MousePositionOnTerrain.Instance.GetWorldPosition;
-        //Vector3 lookAtPos = new Vector3(mousePos.x, drone.transform.position.y, mousePos.z);
-        //drone.transform.LookAt(lookAtPos);
-        drone.transform.LookAt(MousePositionOnTerrain.Instance.GetWorldPosition);
+        transform.LookAt(MousePositionOnTerrain.Instance.GetWorldPosition);
     }
 
     /// <summary>
@@ -178,36 +179,114 @@ public class PlayerMovementController : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        AudioManager.Instance.PlaySound(AudioManager.Sound.Player_Hover, this.transform.position);
+        //NavMeshHit navHit;
+        RaycastHit rayHit;
+        Vector3 oldPos = transform.position;
+        float errorMargin = 0.01f;
+        AudioManager.Instance.PlaySound(AudioManager.Sound.Player_Hover, transform.position);
 
         if (movement != Vector3.zero)
         {
-            drone.transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-            cameraTarget.transform.position = drone.transform.position;
+            transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
         }
 
-        //Toggle gravity if something has pushed the player up above hoverHeight
-        if (rigidbody.useGravity)
+        if (Physics.Raycast(transform.position, Vector3.down, out rayHit, 20, groundLayerMask))
         {
-            if (drone.position.y <= hoverHeight)
+            //RaycastHit cliffRayHit;
+            //Debug.Log($"POD hover height is {rayHit.distance}, default is {defaultHoverHeight}");
+
+            //if (Physics.Raycast(transform.position, Vector3.down, out rayHit, 20, groundLayerMask))
+            //{
+                
+            //}
+            //else
+            //{
+            //    Debug.LogError($"{this}.PlayerMovementController.Move() could not raycast to the ground from position {transform.position}");
+            //}
+
+            //if (NavMesh.SamplePosition(transform.position, out navHit, 4, NavMesh.AllAreas))
+            //{
+            //    Debug.Log($"NavMesh.SamplePosition() from {transform.position} hit mesh at {navHit.position}"); 
+
+            //    if (transform.position.x != navHit.position.x && transform.position.z != navHit.position.z)
+            //    {
+            //        Debug.LogError($"NavHit position {navHit.position} is not in line with {transform.position}. Cancelling movement to that position.");
+            //        transform.position = oldPos;
+            //    }
+            //}
+            //else
+            //{
+            //    Debug.LogError($"Position {transform.position} is not on the nav mesh. Cancelling movement to that position.");
+            //    transform.position = oldPos;
+            //}
+
+            //Toggle gravity if something has pushed the player up above hoverHeight
+            if (rigidbody.useGravity)
             {
-                drone.position = new Vector3(drone.position.x, hoverHeight, drone.position.z);
-                rigidbody.useGravity = false;
-                rigidbody.drag = 100;
+                if (rayHit.distance <= defaultHoverHeight - errorMargin)
+                {
+                    transform.position = new Vector3(transform.position.x, defaultHoverHeight, transform.position.z);
+                    rigidbody.useGravity = false;
+                    rigidbody.drag = 100;
+                    Debug.Log("POD below hover height");
+                }
+                else
+                {
+                    Debug.Log("POD above or at hover height");
+                }
+            }
+            else
+            {
+                if (rayHit.distance > defaultHoverHeight + errorMargin)
+                {
+                    rigidbody.useGravity = true;
+                    rigidbody.drag = 0;
+                    rigidbody.velocity = Vector3.zero;
+                    Debug.Log("POD above hover height");
+                }
+                else
+                {
+                    Debug.Log("POD below  or at hover height");
+                }
             }
         }
         else
         {
-            if (drone.position.y > hoverHeight)   //TODO: account for terrain pushing the player up
-            {
-                rigidbody.useGravity = true;
-                rigidbody.drag = 0;
-                rigidbody.velocity = Vector3.zero;
-            }
+            Debug.LogError($"{this}.PlayerMovementController.Move() could not raycast to the ground from position {transform.position}");
         }
 
-        //Positioning this line here accounts for the player having been moved by an external force (e.g. pushed by enemies)
-        cameraTarget.position = drone.position;     
+        cameraTarget.transform.position = transform.position;
+
+        //AudioManager.Instance.PlaySound(AudioManager.Sound.Player_Hover, this.transform.position);
+
+        //if (movement != Vector3.zero)
+        //{
+        //    drone.transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        //    cameraTarget.transform.position = drone.transform.position;
+        //}
+
+        ////Toggle gravity if something has pushed the player up above hoverHeight
+        //if (rigidbody.useGravity)
+        //{
+        //    if (drone.position.y <= hoverHeight)
+        //    {
+        //        drone.position = new Vector3(drone.position.x, hoverHeight, drone.position.z);
+        //        rigidbody.useGravity = false;
+        //        rigidbody.drag = 100;
+        //    }
+        //}
+        //else
+        //{
+        //    if (drone.position.y > hoverHeight)   //TODO: account for terrain pushing the player up
+        //    {
+        //        rigidbody.useGravity = true;
+        //        rigidbody.drag = 0;
+        //        rigidbody.velocity = Vector3.zero;
+        //    }
+        //}
+
+        ////Positioning this line here accounts for the player having been moved by an external force (e.g. pushed by enemies)
+        //cameraTarget.position = drone.position;     
     }
 
     /// <summary>
