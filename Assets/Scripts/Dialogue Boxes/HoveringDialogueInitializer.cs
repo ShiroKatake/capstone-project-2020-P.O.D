1a256cfdfef83d74b9beaa2e117fe89595301d87
ShiroKatake <tgkhang262@gmail.com>
2020-07-19 00:18:02 +1000

    Finished hovering dialogue box, ready for use o7

---
+++ Assets/Scripts/Dialogue Boxes/HoveringDialogueInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoveringDialogueInitializer : MonoBehaviour
+{
+	[SerializeField] private LayoutGroup layoutGroupComponent;
+
+	private void OnEnable()
+	{
+		StartCoroutine("UpdateLayoutGroup");
+	}
+
+	IEnumerator UpdateLayoutGroup()
+	{
+		layoutGroupComponent.enabled = false;
+		yield return new WaitForEndOfFrame();
+		layoutGroupComponent.enabled = true;
+	}
+}
---
+++ Assets/Scripts/Dialogue Boxes/HoveringDialogueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoveringDialogueManager : MonoBehaviour
+{
+	[SerializeField] private GameObject hoverDialogue;
+	private RectTransform rectTransform;
+
+	private void Awake()
+	{
+		rectTransform = hoverDialogue.GetComponent<RectTransform>();
+		HideDialogue();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			ShowDialogue();
+		}
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			HideDialogue();
+		}
+	}
+
+	public void ShowDialogue()
+	{
+		hoverDialogue.SetActive(true);
+	}
+
+	public void HideDialogue()
+	{
+		hoverDialogue.SetActive(false);
+	}
+}
