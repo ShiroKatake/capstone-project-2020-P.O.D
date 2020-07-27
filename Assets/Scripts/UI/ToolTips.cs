6847c8aec21f9d29a6c1e38c0822d58b647ad966
huf0001 <samhuffer01@gmail.com>
2020-07-27 20:33:30 +1000

    Oops, forgot this bit

--- Assets/Scripts/UI/ToolTips.cs
+++ Assets/Scripts/UI/ToolTips.cs
@@ -17,8 +17,8 @@ public class ToolTips : MonoBehaviour
     {
         IceDrill,
         FusionReactor,
-        Incinorator,
-        Boilier,
+        Incinerator,
+        Boiler,
         Greenhouse,
         Shotgun,
         MachineGun,
