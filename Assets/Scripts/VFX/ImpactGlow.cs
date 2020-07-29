d3c39dfc877b68392483d12f4f5c287d05868b67
ShiroKatake <tgkhang262@gmail.com>
2020-07-29 15:00:49 +1000

    Modified beams to make them pulsate rather than being static

---
+++ Assets/Scripts/VFX/ImpactGlow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactGlow : MonoBehaviour
+{
+	[SerializeField] private LineRenderer lineRenderer;
+	private Vector3[] positions;
+
+	private void Update()
+	{
+		lineRenderer.GetPositions(positions);
+		transform.LookAt(lineRenderer.transform);
+	}
+
+	public void OnMining()
+	{
+
+	}
+}
---
+++ Assets/Scripts/VFX/MiningBeam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningBeam : MonoBehaviour
+{
+	[SerializeField] private Transform beamOrigin;
+	[SerializeField] private Transform beamEnd;
+	private LineRenderer lineRenderer;
+
+	private void Awake()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+
+	void Start()
+    {
+		lineRenderer.SetPosition(0, beamOrigin.position);
+
+	}
+
+    // Update is called once per frame
+    void Update()
+	{
+		lineRenderer.SetPosition(1, beamEnd.position);
+	}
+}
