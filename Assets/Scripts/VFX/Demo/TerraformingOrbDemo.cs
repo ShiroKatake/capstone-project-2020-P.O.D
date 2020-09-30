c523bb5d92c6d9e5b75be5f3d536f17eb3e2ef56
ShiroKatake <tgkhang262@gmail.com>
2020-09-30 13:23:21 +1000

    UPDATE: Added more phases, made explooooshion trigger

--- Assets/Scripts/VFX/Demo/TerraformingOrbDemo.cs
+++ Assets/Scripts/VFX/Demo/TerraformingOrbDemo.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// A demo on how to control the script.
+/// TL;DR:
+/// - To change phase, you need to feed it an int value to terraformingOrbController.CurrentPhase.
+/// - To end the FX in an exploooshion, just execute its Explode() method.
+/// </summary>
 public class TerraformingOrbDemo : MonoBehaviour
 {
+	//The fx's phase is changed by feeding it an int value. Using an enum might help with this
 	private enum TestPhase
 	{
 		Zero,
@@ -11,7 +16,8 @@ public class TerraformingOrbDemo : MonoBehaviour
 		Two,
 		Three,
 		Four,
-		Five
+		Five,
+		Six
 	}
 	
 	[Header("Terraforming Orb")]
@@ -19,18 +25,40 @@ public class TerraformingOrbDemo : MonoBehaviour
 	[SerializeField] private TestPhase setPhase = 0;
 	[SerializeField] private bool beginSequenceDemo;
 
+	// Explode() should only be called ONCE (if it happens in update, make sure to have a bool to prevent multiple calls)
+	[SerializeField] private bool explode;
+	private bool alreadyCalledExplode = false;
+
 	private float timeElapsed;
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
 		if (beginSequenceDemo)
 		{
-			timeElapsed += Time.deltaTime;
-			if (timeElapsed > 5 && terraformingOrbController.IsScaleLerpingFinished && terraformingOrbController.CurrentPhase < 5)
+			if (terraformingOrbController.CurrentPhase < terraformingOrbController.PhaseCount - 2) // - 2 here because last phase is explode, and we wanna manually trigger that
 			{
-				timeElapsed = 0f;
-				terraformingOrbController.CurrentPhase = terraformingOrbController.CurrentPhase + 1;
+				timeElapsed += Time.deltaTime;
+				if (timeElapsed > 5 && terraformingOrbController.IsScaleLerpingFinished)
+				{
+					timeElapsed = 0f;
+					terraformingOrbController.CurrentPhase = terraformingOrbController.CurrentPhase + 1;
+				}
+			}
+		}
+
+		if (explode)
+		{
+			if (!alreadyCalledExplode)
+			{
+				terraformingOrbController.Explode();
+				alreadyCalledExplode = true;
+			}
+			else if (terraformingOrbController.IsShockwaveFinished)
+			{
+				alreadyCalledExplode = false;
+				explode = false;
+				setPhase = 0;
 			}
 		}
 
