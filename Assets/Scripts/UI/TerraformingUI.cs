d13eaf35e58816ff70ccbec2fdda27b726b12ec1
ShiroKatake <tgkhang262@gmail.com>
2020-10-27 17:19:32 +1100

    FIX: An issue where calling updateTargetRatio resulted in null reference error

--- Assets/Scripts/UI/TerraformingUI.cs
+++ Assets/Scripts/UI/TerraformingUI.cs
@@ -3,40 +3,47 @@ using UnityEngine.Events;
 
 public class TerraformingUI : MonoBehaviour
 {
-	protected int[] targetRatioFill;
-	protected int[] currentRatioFill;
+	public int[] targetRatioFill = new int[3];
+	public int[] currentRatioFill = new int[3];
 
-	protected UnityAction updateCurrentRatio;
-	protected UnityAction updateTargetRatio;
+	public UnityAction updateCurrentRatio;
+	public UnityAction updateTargetRatio;
+
+	[SerializeField] TerraformingUIBar bar1;
+	[SerializeField] TerraformingUIBar bar2;
+	[SerializeField] TerraformingUIBar bar3;
+
 
 	public void UpdateCurrent(int[] currentRatio)
 	{
 		currentRatioFill = currentRatio;
-		updateCurrentRatio?.Invoke();
+		updateCurrentRatio.Invoke();
 	}
 
 	public void UpdateTarget(int[] targetRatio, int[] currentRatio)
 	{
 		int multiplier = 1;
 		int currentMultiplier;
-
 		//Find the largest multiplier
 		for (int i = 0; i < targetRatio.Length; i++)
 		{
-			currentMultiplier = Mathf.CeilToInt(targetRatio[i] / currentRatio[i]);
+			currentMultiplier = Mathf.CeilToInt(currentRatio[i] / targetRatio[i]);
 			if (currentMultiplier > multiplier)
 				multiplier = currentMultiplier;
 		}
+		//Debug.Log($"Max Multiplier: {multiplier}");
 
 		//Update the target array
-		targetRatioFill = targetRatio;
+		targetRatioFill[0] = targetRatio[0];
+		targetRatioFill[1] = targetRatio[1];
+		targetRatioFill[2] = targetRatio[2];
 
 		//Multiply each element in the target with that multiplier
 		for (int i = 0; i < targetRatioFill.Length; i++)
 		{
-			targetRatioFill[i] *= multiplier;
+			targetRatioFill[i] = targetRatioFill[i] * multiplier;
 		}
 
-		updateTargetRatio?.Invoke();
+		updateTargetRatio.Invoke();
 	}
 }
