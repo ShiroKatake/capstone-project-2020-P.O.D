2ddee7a250e991bfcd3c1e7a2f9bf059c3bd9594
ShiroKatake <tgkhang262@gmail.com>
2020-10-27 14:29:02 +1100

    ADD: Child script that will update the visuals

---
+++ Assets/Scripts/UI/Bars/TerraformingUIBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public enum BarType
+{
+	Boiler,
+	Greenhouse,
+	Incinerator
+}
+
+public class TerraformingUIBar : TerraformingUI
+{
+	[SerializeField] private BarType bar;
+	[SerializeField] private Image currentFill;
+	[SerializeField] private Image targetFill;
+
+	private int maxBarValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		updateCurrentRatio += UpdateCurrent;
+		updateTargetRatio += UpdateCurrent;
+	}
+
+	private void UpdateTarget(int target, int current)
+	{
+		targetFill.fillAmount = targetRatioFill[(int)bar];
+	}
+
+	private void UpdateCurrent()
+	{
+		currentFill.fillAmount = currentRatioFill[(int)bar] / maxBarValue;
+	}
+}
