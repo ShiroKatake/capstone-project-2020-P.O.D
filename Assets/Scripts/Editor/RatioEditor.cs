d3efbde10880099b14f5e49cfe861bd297c0b1ea
ShiroKatake <tgkhang262@gmail.com>
2020-10-27 14:49:56 +1100

    ADD: Editor script for testing

---
+++ Assets/Scripts/Editor/RatioEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RatioManager))]
+public class RatioEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		RatioManager ratioManager = (RatioManager)target;
+
+		GUILayout.BeginHorizontal();
+
+			if (GUILayout.Button("Get Target Ratio"))
+			{
+				ratioManager.UpdateTargetRatio();
+			}
+
+			if (GUILayout.Button("Get Current Ratio"))
+			{
+				ratioManager.UpdateCurrentRatio();
+			}
+
+		GUILayout.EndHorizontal();
+	}
+}
