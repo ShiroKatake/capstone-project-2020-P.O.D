a08ef752440a97541e992820433a4fe1e8fccf25
ShiroKatake <tgkhang262@gmail.com>
2020-10-29 13:29:35 +1100

    ADD: Script for changing texts on the production screen

---
+++ Assets/Scripts/UI/Terraforming UI/BuildingStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildingStats : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI enabledText;
+	[SerializeField] private TextMeshProUGUI disabledText;
+	[SerializeField] private TextMeshProUGUI requiredText;
+
+	[SerializeField] private EBuilding ratioBuilding;
+
+	public void DisplayBuildingInfo()
+	{
+		enabledText.text = $"Enabled: {BuildingManager.Instance.BuiltAndOperationalBuildingsCount(ratioBuilding)}";
+		disabledText.text = $"Disabled: {BuildingManager.Instance.BuiltAndNonOperationalBuildingsCount(ratioBuilding)}";
+		requiredText.text = $"Required: {TerraformingUI.Instance.TargetRatio((int)ratioBuilding - 6) - BuildingManager.Instance.BuiltAndOperationalBuildingsCount(ratioBuilding)}";
+	}
+}
