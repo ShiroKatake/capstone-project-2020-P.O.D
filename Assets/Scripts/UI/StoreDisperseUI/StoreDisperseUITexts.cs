973f95e2d15a091aa165c370101d1a278c7d2d61
ShiroKatake <tgkhang262@gmail.com>
2020-11-01 18:39:37 +1100

    UPDATE: Added points gained section

--- Assets/Scripts/UI/StoreDisperseUI/StoreDisperseUITexts.cs
+++ Assets/Scripts/UI/StoreDisperseUI/StoreDisperseUITexts.cs
@@ -8,6 +8,7 @@ public class StoreDisperseUITexts : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI header;
 	[SerializeField] TextMeshProUGUI pointsStored;
+	[SerializeField] TextMeshProUGUI pointsGained;
 	[SerializeField] TextMeshProUGUI disperseBonus;
 	[SerializeField] TextMeshProUGUI wavesRemaining;
 
@@ -21,8 +22,9 @@ public class StoreDisperseUITexts : MonoBehaviour
 	private void OnEnable()
 	{
 		header.text = $"Wave {AlienManager.Instance.CurrentWave} complete!";
-		pointsStored.text = $"Points stored: <color=#75FF00>{RatioManager.Instance.PointsStored}</color> complete!";
-		disperseBonus.text = $"Disperse Bonus: <color=#FFB500>+{RatioManager.Instance.DisperseBonus}%</color>";
+		pointsStored.text = $"Points stored: {RatioManager.Instance.PointsStored}";
+		pointsGained.text = $"Points gained: <color=#75FF00>{RatioManager.Instance.PointsGained}</color>";
+		disperseBonus.text = $"Disperse bonus: <color=#FFB500>+{RatioManager.Instance.DisperseBonus}%</color>";
 		wavesRemaining.text = $"Waves remaining: {AlienManager.Instance.WavesRemaining}";
 
 		LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
