011490aedd6f3876671992ffaa916ee2ed709b7c
ShiroKatake <tgkhang262@gmail.com>
2020-11-07 19:33:13 +1100

    FIX: Ratio Bar in building bar not updating on Start

--- Assets/Scripts/UI/Terraforming UI/TerraformingUIBar.cs
+++ Assets/Scripts/UI/Terraforming UI/TerraformingUIBar.cs
@@ -15,7 +15,7 @@ public class TerraformingUIBar : MonoBehaviour
 
 	public void UpdateTargetRatio(int[] ratioArray)
 	{
-        if (targetFill.isActiveAndEnabled)
+        if (targetFill.gameObject.activeInHierarchy)
 			targetFill.fillAmount = ratioArray[(int)bar - 6] * TerraformingUI.Instance.MaxMultiplier / (float)TerraformingUI.Instance.MaxBarValue;
         //Debug.Log($"Value: {(int)bar - 6}");
         //Debug.Log($"Value: {this.gameObject.name}, {(int)bar}");
@@ -25,7 +25,7 @@ public class TerraformingUIBar : MonoBehaviour
 
     public void UpdateCurrentRatio(int[] ratioArray)
 	{
-		if (currentFill.isActiveAndEnabled)
+		if (currentFill.gameObject.activeInHierarchy)
 			currentFill.fillAmount = ratioArray[(int)bar - 6] / (float)TerraformingUI.Instance.MaxBarValue;
         //Debug.Log($"Value: {ratioArray[(int)bar - 6]}");
         //Debug.Log($"Max Value: {TerraformingUI.Instance.MaxMultiplier}");
